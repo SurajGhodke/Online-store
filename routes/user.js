5aa45406935e3a2f16351d9cf3bfa7b8b421d3c8
Suraj Ghodke <surajghodke0102@gmail.com>
2022-09-09 22:21:06 +0530

    admin apis and completed user module

--- routes/user.js
+++ routes/user.js
@@ -9,8 +9,13 @@ const {
   getLoggedInUserDetails,
   changePassword,
   updateUserDetails,
+  adminAllUser,
+  managerAllUser,
+  admingetOneUser,
+  adminUpdateOneUserDetails,
+  adminDeleteOneUser,
 } = require("../controllers/userController");
-const { isLoggedIn } = require("../middlewares/user");
+const { isLoggedIn, customRole } = require("../middlewares/user");
 
 router.route("/signup").post(signup);
 router.route("/login").post(login);
@@ -20,5 +25,18 @@ router.route("/password/reset/:token").post(passwordReset);
 router.route("/userdashboard").get(isLoggedIn, getLoggedInUserDetails);
 router.route("/password/update").post(isLoggedIn, changePassword);
 router.route("/userdashboard/update").post(isLoggedIn, updateUserDetails);
+
+//admin only routes
+router.route("/admin/users").get(isLoggedIn, customRole("admin"), adminAllUser);
+router
+  .route("/admin/user/:id")
+  .get(isLoggedIn, customRole("admin"), admingetOneUser)
+  .put(isLoggedIn, customRole("admin"), adminUpdateOneUserDetails)
+  .delete(isLoggedIn, customRole("admin"), adminDeleteOneUser);
+
+// manager only route
+router
+  .route("/manager/users")
+  .get(isLoggedIn, customRole("manager"), managerAllUser);
 
 module.exports = router;

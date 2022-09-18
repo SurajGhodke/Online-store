48a851056d169f561dacf3c0adb406707c45a426
Suraj Ghodke <surajghodke0102@gmail.com>
2022-09-18 15:25:20 +0530

    get product list, delete, update, get single

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,7 +2,7 @@ const Product = require("../models/product");
 const BigPromise = require("../middlewares/bigPromise");
 const CustomError = require("../utils/customError");
 const cloudinary = require("cloudinary");
-// const WhereClause = require("../utils/whereClause");
+const WhereClause = require("../utils/whereClause");
 
 exports.addProduct = BigPromise(async (req, res, next) => {
   // images
@@ -37,5 +37,116 @@ exports.addProduct = BigPromise(async (req, res, next) => {
   res.status(200).json({
     success: true,
     product,
+  });
+});
+
+exports.getAllProduct = BigPromise(async (req, res, next) => {
+  const resultPerPage = 6;
+  const totalcountProduct = await Product.countDocuments();
+
+  const productsObj = new WhereClause(Product.find(), req.query)
+    .search()
+    .filter();
+
+  let products = await productsObj.base;
+  const filteredProductNumber = products.length;
+
+  //products.limit().skip()
+
+  productsObj.pager(resultPerPage);
+  products = await productsObj.base.clone();
+
+  res.status(200).json({
+    success: true,
+    products,
+    filteredProductNumber,
+    totalcountProduct,
+  });
+});
+
+exports.getOneProduct = BigPromise(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new CustomError("No product found with this id", 401));
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+// admin only controllers
+exports.adminGetAllProduct = BigPromise(async (req, res, next) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
+});
+
+exports.adminUpdateOneProduct = BigPromise(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new CustomError("No product found with this id", 401));
+  }
+  let imagesArray = [];
+
+  if (req.files) {
+    //destroy the existing image
+    for (let index = 0; index < product.photos.length; index++) {
+      const res = await cloudinary.v2.uploader.destroy(
+        product.photos[index].id
+      );
+    }
+
+    for (let index = 0; index < req.files.photos.length; index++) {
+      let result = await cloudinary.v2.uploader.upload(
+        req.files.photos[index].tempFilePath,
+        {
+          folder: "products", //folder name -> .env
+        }
+      );
+
+      imagesArray.push({
+        id: result.public_id,
+        secure_url: result.secure_url,
+      });
+    }
+  }
+
+  req.body.photos = imagesArray;
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+exports.adminDeleteOneProduct = BigPromise(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new CustomError("No product found with this id", 401));
+  }
+
+  //destroy the existing image
+  for (let index = 0; index < product.photos.length; index++) {
+    const res = await cloudinary.v2.uploader.destroy(product.photos[index].id);
+  }
+
+  await product.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "Product was deleted !",
   });
 });
--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const { addProduct } = require("../controllers/productController");
+const {
+  addProduct,
+  getAllProduct,
+  getOneProduct,
+  adminGetAllProduct,
+  adminUpdateOneProduct,
+  adminDeleteOneProduct,
+} = require("../controllers/productController");
 const router = express.Router();
 const { isLoggedIn, customRole } = require("../middlewares/user");
 
 //user routes
-// router.route("/products").get(getAllProduct);
-// router.route("/product/:id").get(getOneProduct);
+router.route("/products").get(getAllProduct);
+router.route("/product/:id").get(getOneProduct);
 // router.route("/review").put(isLoggedIn, addReview);
 // router.route("/review").delete(isLoggedIn, deleteReview);
 // router.route("/reviews").get(isLoggedIn, getOnlyReviewsForOneProduct);
@@ -15,13 +22,13 @@ router
   .route("/admin/product/add")
   .post(isLoggedIn, customRole("admin"), addProduct);
 
-// router
-//   .route("/admin/products")
-//   .get(isLoggedIn, customRole("admin"), adminGetAllProduct);
+router
+  .route("/admin/products")
+  .get(isLoggedIn, customRole("admin"), adminGetAllProduct);
 
-// router
-//   .route("/admin/product/:id")
-//   .put(isLoggedIn, customRole("admin"), adminUpdateOneProduct)
-//   .delete(isLoggedIn, customRole("admin"), adminDeleteOneProduct);
+router
+  .route("/admin/product/:id")
+  .put(isLoggedIn, customRole("admin"), adminUpdateOneProduct)
+  .delete(isLoggedIn, customRole("admin"), adminDeleteOneProduct);
 
 module.exports = router;
